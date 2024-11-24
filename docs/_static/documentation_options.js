a450d5d2e5b5445b6b520998823a4235c58a661f
Brian Warner <brian@bdwarner.com>
2024-11-23 21:04:29 -0500

    Convert images in documentation to static URLs
    Signed-off-by: Brian Warner <brian@bdwarner.com>

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev1+g2549980',
+    VERSION: '0.0.post1.dev1+g049e3b8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
